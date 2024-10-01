2cb959bf69052039bd217c6c5234f329a4061637
Noman <pxycknomdictator@outlook.com>
2024-10-01 11:15:15 +0500

    added total tasks

--- src/components/Clear.jsx
+++ src/components/Clear.jsx
@@ -1,7 +1,10 @@
 export const Clear = () => {
   return (
-    <section className="flex w-full px-4 c_small:px-0 items-center justify-center mt-10 absolute responsive_btn c_small:bottom-[-29px] c_small:relative">
-      <button className="w-full py-2 text-xl text-white bg-indigo-500 font-semibold rounded-md hover:bg-indigo-600">
+    <section className="w-full px-4 c_small:px-0 items-center justify-center mt-12 absolute responsive_btn c_small:bottom-[-29px] c_small:relative">
+      <span className="text-xl font-semibold inline-block mb-2 text-gray-500">
+        Total Todos: 4
+      </span>
+      <button className="inline-block w-full py-2 text-xl text-white bg-indigo-500 font-semibold rounded-md hover:bg-indigo-600">
         Clear all
       </button>
     </section>
