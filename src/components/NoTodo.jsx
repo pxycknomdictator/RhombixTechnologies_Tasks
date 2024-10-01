726eedb5f2ac0ad08d3e10fac6a795f0491e00b8
Noman <pxycknomdictator@outlook.com>
2024-10-01 14:17:09 +0500

    added no task component

---
+++ src/components/NoTodo.jsx
@@ -0,0 +1,5 @@
+export const NoTodo = () => {
+  return (
+    <h1 className="text-3xl text-center font-semibold">All Tasks Completed</h1>
+  );
+};
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { Todo } from "./Todo";
+import { NoTodo } from "./NoTodo";
 import { todoContext } from "../store/todoContext";
 
 export const TodoList = () => {
@@ -7,9 +8,11 @@ export const TodoList = () => {
 
   return (
     <ul className="mt-8 grid grid-cols-1 gap-y-5">
-      {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} />
-      ))}
+      {todos.length > 0 ? (
+        todos.map((todo) => <Todo key={todo.id} todo={todo} />)
+      ) : (
+        <NoTodo />
+      )}
     </ul>
   );
 };
